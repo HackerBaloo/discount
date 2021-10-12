8b689b188a4f9afebb0ea01ff4f2c574f1a81970
Björn Carlsson <b.r.carlsson@gmail.com>
2021-10-12 19:20:36 +0200

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-python3 -m venv venv
-echo "run: . ./venv/bin/activate"
-pip install nameko-redis
-docker run -d -p 5672:5672 --hostname nameko-rabbitmq rabbitmq:3
-docker run --name my-redis-container -d redis
-nameko run messagequeue
